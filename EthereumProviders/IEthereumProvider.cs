882d801df5f8aebea8a07f9e46e47738fbcd1d86
argon666 <konrad.korczak18@gmail.com>
2021-03-26 15:33:05 +0100

    EthereumProviderLibrary
    Library with component able to connect to metamask

---
+++ EthereumProviders/IEthereumProvider.cs
@@ -0,0 +1,20 @@
+﻿using Nethereum.Web3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EthereumProviders
+{
+    public interface IEthereumProvider
+    {
+        public bool Available { get; }
+        public string Account { get; }
+        public string Network { get; }
+        public string NameOfProvider { get; }
+        public Task<bool> IsProviderInstalled();
+        public Task<string> EnableProviderAndGetAddress();
+        public Web3 GetWeb3Async();
+    }
+}
